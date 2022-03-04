5bf6eedda7329f7d979db39dfe736db5f46790e9
Daniel Kneale <82185529+Crimson-Beast@users.noreply.github.com>
2022-03-04 08:38:01 -0800

    installed dependencies, created scripts, set up index, server file wip

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // build your server here and require it from index.js
+const express = require("express")
+
+const projectRoute = require(("./project/router"))
+const resourceRoute = require(("./resource/router"))
+const taskRoute = require(("./task/router"))
+
+const server = express()
+
+server.use(express.json())
+
+server.use("/api/project")
